6290da10eb09a01b1dececd287fbb60943675e85
Yunhwan Jeon <yunsol267@gmail.com>
2020-07-15 01:53:01 +0900

    긴급 재난문자 상세보기 구현

--- public/assets/more-emergency.js
+++ public/assets/more-emergency.js
@@ -1,21 +1,18 @@
 function reqListener () {
-    var currentDate = new Date();
-    var mydata = JSON.parse(this.responseText);
-
-    document.getElementById("emergency-preview-1").innerHTML =
-    mydata['emergency'][0].title;
-
-    txt += "<table border='1'>"
-
-    for (x in mydata) {
-      txt += "<tr><td>" + mydata[x].msg + "</td></tr>";
-    }
-
-    txt += "</table>"
-    
-    document.getElementById("demo").innerHTML = txt;
-
+  var mydata = JSON.parse(this.responseText);
+  txt = "";
+  txt += "<table border='1'>"
+  txt += "<tr><th>발송 지역</th><th>메시지 내용</th><th>발송 일시</th>"
+  for (x in mydata['emergency']) {
+    txt += "<tr><td>" + mydata['emergency'][x].title + "</td>";
+    txt += "<td>" + mydata['emergency'][x].msg + "</td>";
+    txt += "<td>" + mydata['emergency'][x].date + "</td></tr>";
+  }
+  txt += "</tr>"
+  txt += "</table>"    
+  document.getElementById("emergency").innerHTML = txt;
 }
+
 var oReq = new XMLHttpRequest();
 oReq.addEventListener("load", reqListener);
 oReq.open("GET", "/emergency.json");
